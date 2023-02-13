98e4bf53ec5c7fd1b32b9b197edc931ab4205b94
dmitriyshikov <dshikov@alfabank.ru>
2023-02-14 00:51:39 +0400

    Initial commit

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceViewModel2: UIImageView!
+    @IBOutlet weak var diceVIewModel1: UIImageView!
+    @IBOutlet weak var rollButton: UIButton!
+    var array: Array<UIImage>? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initArray()
+        rollButton.backgroundColor = UIColor(red: 0.4, green: 1.0, blue: 0.2, alpha: 0.5)
+        rollButton.addTarget(self, action: #selector(tapRollButton(_:)), for: UIControl.Event.touchUpInside)
     }
 
-
+    
+    private func initArray() {
+        var imageViewList = Array<UIImage>()
+        imageViewList.append(UIImage(imageLiteralResourceName: "DiceOne"))
+        imageViewList.append(UIImage(imageLiteralResourceName: "DiceTwo"))
+        imageViewList.append(UIImage(imageLiteralResourceName: "DiceThree"))
+        imageViewList.append(UIImage(imageLiteralResourceName: "DiceFour"))
+        imageViewList.append(UIImage(imageLiteralResourceName: "DiceFive"))
+        imageViewList.append(UIImage(imageLiteralResourceName: "DiceSix"))
+        self.array = imageViewList
+    }
+    
+    @IBAction func tapRollButton(_ sender: UIButton) {
+        diceVIewModel1.image = array?.randomElement()
+        diceViewModel2.image = array?.randomElement()
+    }
 }
 
